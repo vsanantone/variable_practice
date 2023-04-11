6875fed1772278325bf503d96c7892f3075a8caa
Victor Sanchez <vsanantone@gmail.com>
2023-04-11 17:53:32 -0500

    Add three put statements

--- variables.rb
+++ variables.rb
@@ -1,14 +1,25 @@
-string1 = "The world is safer with Yeshua."
-string2 = "Avram is the father of tzedakah."
-string3 = " "
+quote1 = "The world is safer with Yeshua."
+quote2 = "Avram is the father of tzedakah."
+quote3 = " "
 
-integer1 = 45
-integer2 = 48
-integer3 = 92
+age1 = 45
+age2 = 48
+age3 = 92
 
-float1 = 1200.00
-float2 = 45.69
-float3 = 22.23
+total_saved1 = 1200.00
+total_saved2 = 45.69
+total_saved3 = 22.23
 
-boolean1 = true
-boolean2 = false
+happy1 = true
+happy2 = false
+
+quote1 = "The shining one is brighter than the noon day sun."
+quote2 = "A liar sings a sad song."
+quote3 = "Joy"
+
+
+puts "John is #{age1} years old."
+
+puts "My favorite quote is #{quote3}! "
+
+puts "A prosperuos man is very #{happy1} indeed!"
